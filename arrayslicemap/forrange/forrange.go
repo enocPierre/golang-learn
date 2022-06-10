fd0ce3a405b2a7a69cadccf7506d4a9180c3d4aa
enocPierre <enoc.pierre@mercadolivre.com>
2022-06-09 22:09:47 -0300

    switch,array, for

---
+++ arrayslicemap/forrange/forrange.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+
+func main() {
+	numeros := [...] int {1,2,3,4,5} // compilador contas!
+
+	for i, numero := range numeros {
+		fmt.Println("%d) %d\n", i, numero)
+	}
+
+	for num := range numeros {
+		fmt.Println(num)
+	}
+	for _, num := range numeros {
+		fmt.Println(num)
+	}
+}
